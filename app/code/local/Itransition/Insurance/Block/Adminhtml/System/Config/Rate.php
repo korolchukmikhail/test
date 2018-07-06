b6d49478ad157273b5a9eaa77ee7236cad14d4a9
Korolchuk Mikhail <korolchuk.mikhail@gmail.com>
2018-07-06 12:40:56 +0300

    save data, set fields in system config

--- app/code/local/Itransition/Insurance/Block/Adminhtml/System/Config/Rate.php
+++ app/code/local/Itransition/Insurance/Block/Adminhtml/System/Config/Rate.php
@@ -7,45 +7,60 @@
  */
 
 class Itransition_Insurance_Block_Adminhtml_System_Config_Rate extends Mage_Adminhtml_Block_System_Config_Form_Field {
+    protected $_rates = [];
+
     /**
      * @param Varien_Data_Form_Element_Abstract $element
      * @return string
      */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element) {
-        /** @var Itransition_Insurance_Model_Shipping $shipping**/
+        $this->setElement($element);
+
+        /** @var Itransition_Insurance_Model_Shipping $shipping * */
         $shipping = Mage::getModel('it_insurance/shipping');
-        $shipping_methods = $shipping->getActiveMethods();
+        $this->_rates = $shipping->getRates();
+        $shipping_carriers = $shipping->getCarriers();
 
         $html = '<div class="itSystemConfigInsurance">';
-        foreach ($shipping_methods as $shipping_method) {
-            $html .= $this->_getRow($shipping_method);
+        foreach ($shipping_carriers as $shipping_carrier) {
+            $html .= $this->_getMethods($shipping_carrier);
         }
         $html .= '</div>';
 
         return $html;
     }
 
-    private function _getRow($method){
+    private function _getMethods($carrier) {
         $row = '';
-        if(!empty($method['value'])) {
+        if (!empty($carrier['value'])) {
             $row = '<div class="itSystemConfigInsurance__row">';
-            $row .= '<div class="itSystemConfigInsurance__titleCarrier">'.$method['label'].'</div>';
-            $row .= '<div class="itSystemConfigInsurance__shippingMethods">';
-            foreach ($method['value'] as $item) {
-                $row .= '<div class="itSystemConfigInsurance__titleShippingMethod">'.$item['label'].'</div>';
-                $row .= '<label for="" class="itSystemConfigInsurance__state">'.Mage::helper('adminhtml')->__('Enabled').'<select>';
-                $row .= '<option value="0">'.Mage::helper('adminhtml')->__('no').'</option>';
-                $row .= '<option value="1">'.Mage::helper('adminhtml')->__('yes').'</option></select></label>';
-                $row .= '<label for="" class="itSystemConfigInsurance__state">'.Mage::helper('adminhtml')->__('Percent?').'<select>';
-                $row .= '<option value="0">'.Mage::helper('adminhtml')->__('no').'</option>';
-                $row .= '<option value="1">'.Mage::helper('adminhtml')->__('yes').'</option></select></label>';
-                $row .= '<label for="" class="itSystemConfigInsurance__value">'.Mage::helper('adminhtml')->__('Rate');
-                $row .= '<input type="text" name="" value=""></label>';
-                $row .= '<input type="hidden" name="" value="'.$item['value'].'">';
+            $row .= '<div class="itSystemConfigInsurance__titleCarrier">' . $carrier['label'] . '</div>';
+            $row .= '<table class="itSystemConfigInsurance__shippingMethods"><tbody>';
+            foreach ($carrier['value'] as $method) {
+                $row .= '<tr>';
+                $row .= '<td class="itSystemConfigInsurance__titleShippingMethod">' . $method['label'] . '</td>';
+                $row .= '<td><label><span>' . Mage::helper('adminhtml')->__('Enabled') . '</span><select name="' . $this->getElement()->getName() . '[state][]" class="itSystemConfigInsurance__select">';
+                $row .= '<option value="0" ' . ($this->_getValue($method['value'], 'state') ? '' : 'selected') . '>' . Mage::helper('adminhtml')->__('No') . '</option>';
+                $row .= '<option value="1" ' . ($this->_getValue($method['value'], 'state') ? 'selected' : '') . '>' . Mage::helper('adminhtml')->__('Yes') . '</option></select></label></td>';
+                $row .= '<td><label><span>' . Mage::helper('adminhtml')->__('Percent?') . '</span><select name="' . $this->getElement()->getName() . '[percent][]" class="itSystemConfigInsurance__select">';
+                $row .= '<option value="0" ' . ($this->_getValue($method['value'], 'percent') ? '' : 'selected') . '>' . Mage::helper('adminhtml')->__('No') . '</option>';
+                $row .= '<option value="1" ' . ($this->_getValue($method['value'], 'percent') ? 'selected' : '') . '>' . Mage::helper('adminhtml')->__('Yes') . '</option></select></label></td>';
+                $row .= '<td><label><span>' . Mage::helper('adminhtml')->__('Rate');
+                $row .= '</span><input type="text" name="' . $this->getElement()->getName() . '[rate][]" class="itSystemConfigInsurance__input" value="' . $this->_getValue($method['value'], 'rate') . '"></label>';
+                $row .= '<input type="hidden" name="' . $this->getElement()->getName() . '[value][]" value="' . $method['value'] . '"></td>';
+                $row .= '</tr>';
             }
-            $row .= '</div></div>';
+            $row .= '</tbody></table></div>';
         }
 
         return $row;
+    }
+
+    private function _getValue($method_code, $field) {
+        if ($this->_rates && isset($this->_rates[$method_code]) && isset($this->_rates[$method_code][$field])) {
+            return $this->_rates[$method_code][$field];
+        }
+
+        return 0;
     }
 }
--- app/code/local/Itransition/Insurance/Model/Shipping.php
+++ app/code/local/Itransition/Insurance/Model/Shipping.php
@@ -8,26 +8,42 @@
 
 class Itransition_Insurance_Model_Shipping extends Mage_Core_Model_Abstract {
 
-    public function getActiveMethods() {
+    public function getCarriers() {
         $methods = Mage::getSingleton('shipping/config')->getActiveCarriers();
 
-        $options = array();
+        $options = [];
 
         foreach ($methods as $_ccode => $_carrier) {
-            $_methodOptions = array();
+            $_method_options = [];
             if ($_methods = $_carrier->getAllowedMethods()) {
                 foreach ($_methods as $_mcode => $_method) {
                     $_code = $_ccode . '_' . $_mcode;
-                    $_methodOptions[] = ['value' => $_code, 'label' => $_method];
+                    $_method_options[] = ['value' => $_code, 'label' => $_method];
                 }
 
                 if (!$_title = Mage::getStoreConfig("carriers/$_ccode/title"))
                     $_title = $_ccode;
 
-                $options[] = ['value' => $_methodOptions, 'label' => $_title];
+                $options[] = ['value' => $_method_options, 'label' => $_title];
             }
         }
 
         return $options;
+    }
+
+    public function getRates() {
+        $rates = [];
+        if ($rates_source = Mage::getStoreConfig('insurance/config/rates')) {
+            $rates_source = unserialize($rates_source);
+            foreach ($rates_source['value'] as $i => $method_code) {
+                $rates[$method_code] = [
+                    'state' => (int)$rates_source['state'][$i],
+                    'percent' => (int)$rates_source['percent'][$i],
+                    'rate' => (float)$rates_source['rate'][$i]
+                ];
+            }
+        }
+
+        return $rates;
     }
 }
